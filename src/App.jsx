b032971532c91a5ff8ec343afc1acbfc8d87df14
hamza <hamza786504@gmail.com>
2024-07-22 03:07:30 +0500

    link swapped with click me anchor -2

--- src/App.jsx
+++ src/App.jsx
@@ -65,29 +65,29 @@ const App = () => {
       },
     ]);
 
-    // const handleKeyDown = (e) => {
-    //   if (e.keyCode === 123) {
-    //     e.preventDefault();
-    //   }
-    //   if (e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)) {
-    //     e.preventDefault();
-    //   }
-    //   if (e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)) {
-    //     e.preventDefault();
-    //   }
-    //   if (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) {
-    //     e.preventDefault();
-    //   }
-    //   if (e.ctrlKey && e.shiftKey && e.keyCode === 'C'.charCodeAt(0)) {
-    //     e.preventDefault();
-    //   }
-    // };
+    const handleKeyDown = (e) => {
+      if (e.keyCode === 123) {
+        e.preventDefault();
+      }
+      if (e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)) {
+        e.preventDefault();
+      }
+      if (e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)) {
+        e.preventDefault();
+      }
+      if (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) {
+        e.preventDefault();
+      }
+      if (e.ctrlKey && e.shiftKey && e.keyCode === 'C'.charCodeAt(0)) {
+        e.preventDefault();
+      }
+    };
 
-    // document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keydown', handleKeyDown);
 
-    // return () => {
-    //   document.removeEventListener('keydown', handleKeyDown);
-    // };
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
   }, []);
 
 
